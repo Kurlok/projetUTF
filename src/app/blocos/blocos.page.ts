6ba6757685d07fd143211229f77584dfa98aa8d0
Jhonatan <jhonnkonopp@gmail.com>
2019-05-28 00:27:12 -0300

    Segundo commit
    -Arrumei as cores;
    -Adicionado Menu;
    -Páginas Home e Blocos

--- src/app/blocos/blocos.page.ts
+++ src/app/blocos/blocos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-blocos',
@@ -13,3 +14,22 @@ export class BlocosPage implements OnInit {
   }
 
 }
+
+export class Menu {
+
+  constructor(private menu: MenuController) { }
+  
+    openFirst() {
+      this.menu.enable(true, 'first');
+      this.menu.open('first');
+    }
+  
+    openEnd() {
+      this.menu.open('end');
+    }
+  
+    openCustom() {
+      this.menu.enable(true, 'custom');
+      this.menu.open('custom');
+    }
+  }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from '@ionic/angular';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,4 +8,23 @@ import { NavController, NavParams } from '@ionic/angular';
 })
 export class HomePage {
 
+}
+
+export class Menu {
+
+constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 }
