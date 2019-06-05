29ceab9da5a99af2a7452e0434a79a8f9b25f9a2
Kurlok <felipe.augum@gmail.com>
2019-06-05 05:12:31 -0300

    Início implementação geolocation.watchPosition()

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -19,121 +19,110 @@ export class HomePage implements OnInit {
 
   @ViewChild('map') mapElement: ElementRef;
   map: any;
-  address:string;
-  userEmail: string;
-
+  address: string;
   constructor(
     private geolocation: Geolocation,
     private nativeGeocoder: NativeGeocoder,
     private navCtrl: NavController,
     private blocosService: BlocosService
-  ) {}
+  ) { 
 
-  ngOnInit(){
+  }
 
-
+  ngOnInit() {
     this.loadMap();
   }
-
+  
   loadMap() {
 
-    this.geolocation.getCurrentPosition().then((resp) => {
-      let latLng = new google.maps.LatLng(-25.051196, -50.132609);
-      let mapOptions = {
-        center: latLng,
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      }
- 
-      this.getAddressFromCoords(-25.051196, -50.132609);
- 
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
- 
-      this.map.addListener('tilesloaded', () => {
-        console.log('accuracy',this.map);
-        this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+    let watch = this.geolocation.watchPosition();
+
+    let latLongCampus = new google.maps.LatLng(-25.051196, -50.132609);
+    let mapOptions = {
+      center: latLongCampus,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false,
+      streetViewControl: false,
+      fullscreenControl: false
+    }
+
+   // this.getAddressFromCoords(-25.051196, -50.132609);
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      //Coloca os marcadores dos blocos no mapa
+      var latLong;
+      this.blocosService.readBlocos().subscribe(data => {
+        this.blocos = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            nome: e.payload.doc.data()['nome'],
+            descricao: e.payload.doc.data()['descricao'],
+            latitude: e.payload.doc.data()['latitude'],
+            longitude: e.payload.doc.data()['longitude'],
+            raio: e.payload.doc.data()['raio']
+          };
+        })
+        for (var i = 0; i < this.blocos.length; i++) {
+          latLong = new google.maps.LatLng(this.blocos[i].latitude, this.blocos[i].longitude);
+          var marker = new google.maps.Marker({
+            position: latLong,
+            map: this.map,
+            title: this.blocos[i].nome
+          });
+          console.log(this.blocos);
+        }
       });
 
-  var latLong;
+    //this.geolocation.getCurrentPosition().then((resp) => {
+     // let latLngAtualiza = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude)
+     // this.map.setCenter(latLngAtualiza);
+     // this.map.addListener('tilesloaded', () => {
+      //  console.log('accuracy', this.map);
+      //  this.getAddressFromCoords(this.map.center.lat(), this.map.center.lng())
+     // });
+    // }).catch((error) => {
+    //   console.log('Erro buscando a sua localização', error);
+    // });
+      
+    //Observa a posição atual do dispositivo e cria marcador
+    this.geolocation.watchPosition().subscribe((position) => {
+      let latLng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      let marker = new google.maps.Marker({
+        map: this.map,
+      //  icon:  'assets/imgs/pins/redpin.png',
+        position: latLng,
+        enableHighAccuracy:true
+       });
+      let message = "Você!!!";
+  }, (err) => {console.log(err);} );
 
-  this.blocosService.readBlocos().subscribe(data => {
-    this.blocos = data.map(e => {
-     return {
-       id: e.payload.doc.id,
-       nome: e.payload.doc.data()['nome'],
-       descricao: e.payload.doc.data()['descricao'],
-       latitude: e.payload.doc.data()['latitude'],
-       longitude: e.payload.doc.data()['longitude'],
-       raio: e.payload.doc.data()['raio']
-     };
-   })
- for (var i = 0; i < this.blocos.length; i++) {
-  latLong = new google.maps.LatLng(this.blocos[i].latitude, this.blocos[i].longitude); 
-  var marker = new google.maps.Marker({
-   position: latLong,
-   map: this.map,
-   title: 'this.blocos.nome.toString()',i
-  });
-  console.log(this.blocos);
-  console.log(this.blocos[i].nome);
-}
-
-
-
-  });  
-
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
   }
-  
+
   getAddressFromCoords(lattitude, longitude) {
-    console.log("getAddressFromCoords "+lattitude+" "+longitude);
+    console.log("getAddressFromCoords " + lattitude + " " + longitude);
     let options: NativeGeocoderOptions = {
       useLocale: true,
       maxResults: 5
     };
-
-
 
     this.nativeGeocoder.reverseGeocode(lattitude, longitude, options)
       .then((result: NativeGeocoderReverseResult[]) => {
         this.address = "";
         let responseAddress = [];
         for (let [key, value] of Object.entries(result[0])) {
-          if(value.length>0)
-          responseAddress.push(value);
- 
+          if (value.length > 0)
+            responseAddress.push(value);
         }
         responseAddress.reverse();
         for (let value of responseAddress) {
-          this.address += value+", ";
+          this.address += value + ", ";
         }
         this.address = this.address.slice(0, -2);
       })
-      .catch((error: any) =>{ 
+      .catch((error: any) => {
         this.address = "Address Not Available!";
       });
- 
   }
-}
-
-export class Menu {
-
-constructor(private menu: MenuController) { }
-
-  openFirst() {
-    this.menu.enable(true, 'first');
-    this.menu.open('first');
-  }
-
-  openEnd() {
-    this.menu.open('end');
-  }
-
-  openCustom() {
-    this.menu.enable(true, 'custom');
-    this.menu.open('custom');
-  }
-
 }
